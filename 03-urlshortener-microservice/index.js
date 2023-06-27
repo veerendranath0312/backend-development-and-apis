feed6fd9f5231731bb7a26d8f75ea681c462960c
Veerendranath <veerendranath0312@gmail.com>
2023-06-27 10:35:26 +0530

    Update solution for project 3

--- 03-urlshortener-microservice/index.js
+++ 03-urlshortener-microservice/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const dns = require('dns')
+const dns = require('dns').promises
 const app = express()
 
 // Basic Configuration
@@ -17,18 +17,48 @@ app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html')
 })
 
+const shorturls = []
+
 // Your first API endpoint
 app.get('/api/hello', (req, res) => {
   res.json({ greeting: 'hello API' })
 })
 
-app.post('/api/shorturl', (req, res) => {
+app.post('/api/shorturl', async (req, res) => {
   const { url } = req.body
 
-  res.status(200).json({
-    original_url: url,
-    short_url: '',
-  })
+  // Using the URL constructor to get the details of an endpoint
+  const siteDetails = new URL(url)
+
+  if (!['http:', 'https:'].includes(siteDetails.protocol)) {
+    return res.json({ error: 'invalid url' })
+  }
+
+  const hostname = siteDetails.host
+
+  try {
+    const options = { all: true }
+    await dns.lookup(hostname, options)
+
+    const newShorturl = { id: shorturls.length + 1, url }
+
+    shorturls.push(newShorturl)
+
+    res.status(200).json({
+      original_url: newShorturl.url,
+      short_url: newShorturl.id,
+    })
+  } catch (error) {
+    res.json({ error: 'invalid url' })
+  }
+})
+
+app.get('/api/shorturl/:id', (req, res) => {
+  const { id } = req.params
+
+  const foundUrl = shorturls.find((shorturl) => shorturl.id === Number(id))
+
+  res.redirect(foundUrl.url)
 })
 
 app.listen(port, () => {
