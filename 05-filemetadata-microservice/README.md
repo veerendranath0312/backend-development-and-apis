2f99fe9dabb247a54be1c2179cb396d2a8265b99
Veerendranath <veerendranath0312@gmail.com>
2023-07-05 00:21:42 +0530

    Update solution for project 5

--- 05-filemetadata-microservice/README.md
+++ 05-filemetadata-microservice/README.md
@@ -1,3 +1,15 @@
 # File Metadata Microservice
 
-This is the boilerplate for the File Metadata Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice
+This is my solution to the File Metadata Microservice project from freeCodeCamp. Instructions for building the project can be found [here](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice)
+
+## Start the application locally
+
+To start the application:
+
+```bash
+# Install dependancies
+$ npm install
+
+# Start the application
+$ npm run dev
+```
--- 05-filemetadata-microservice/index.js
+++ 05-filemetadata-microservice/index.js
@@ -1,15 +1,39 @@
-var express = require('express')
-var cors = require('cors')
+const express = require('express')
+const cors = require('cors')
+const multer = require('multer')
 require('dotenv').config()
 
-var app = express()
+const app = express()
 const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use('/public', express.static(process.cwd() + '/public'))
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html')
+})
+
+// Configuration for multer
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public')
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1]
+    cb(null, `files/admin-${file.fieldname}-${Date.now()}.${ext}`)
+  },
+})
+
+const upload = multer({ storage: multerStorage })
+
+app.post('/api/fileanalyse', upload.single('upfile'), async (req, res) => {
+  console.log(req.file)
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  })
 })
 
 app.listen(port, () => {
